a75aa08c6ea7522fd02b28c7404687acdc28b9d8
hctrgnzlz <hectorgzz89@gmail.com>
2020-06-23 19:26:35 -0400

    Updates

---
+++ precache-manifest.bccff246ef97bc75e45de6258ae10588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c0628af76f21b530aeff9df1e675b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77589ab3ba6a93df2e4c",
+    "url": "/static/css/main.a2f3ad5e.chunk.css"
+  },
+  {
+    "revision": "866120897b728f0f9f06",
+    "url": "/static/js/2.6dae4848.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/static/js/2.6dae4848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77589ab3ba6a93df2e4c",
+    "url": "/static/js/main.c41ae990.chunk.js"
+  },
+  {
+    "revision": "c26a7a7b457983b809a2",
+    "url": "/static/js/runtime-main.a3a47213.js"
+  }
+]);
