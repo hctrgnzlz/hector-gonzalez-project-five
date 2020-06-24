ae843ac7446fce150ae2fc04f0af970ff3820e47
hctrgnzlz <hectorgzz89@gmail.com>
2020-06-24 13:51:58 -0400

    Updates

---
+++ precache-manifest.c36dacf219805794e76ecc00bcb0b2d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659be396f9ea0f82b7a008a2881c552c",
+    "url": "/hector-gonzalez-project-five/index.html"
+  },
+  {
+    "revision": "19495808189e39d34f9e",
+    "url": "/hector-gonzalez-project-five/static/css/main.95f07a8e.chunk.css"
+  },
+  {
+    "revision": "16d691d5878a4977699f",
+    "url": "/hector-gonzalez-project-five/static/js/2.35e7a73a.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/hector-gonzalez-project-five/static/js/2.35e7a73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19495808189e39d34f9e",
+    "url": "/hector-gonzalez-project-five/static/js/main.e6ef01dd.chunk.js"
+  },
+  {
+    "revision": "09ffe30fc94f15945c09",
+    "url": "/hector-gonzalez-project-five/static/js/runtime-main.2571f310.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c274d8ca2280b87591fdaa7be036e95f.js"
+  "/hector-gonzalez-project-five/precache-manifest.c36dacf219805794e76ecc00bcb0b2d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hector-gonzalez-project-five/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
